ef61bf09c462c63fa8da829417417fa6f3df1fd2
klequis <klequis@gmail.com>
2019-02-12 21:23:44 -0800

    added search box to doc

--- src/removeObjectFromArray.js
+++ src/removeObjectFromArray.js
@@ -1,13 +1,6 @@
 import { remove } from 'ramda'
 
-export /**
- *
- *
- * @param { array } array - an array of objects
- * @param { field } field - the object field used for lookup
- * @param { * } value - the value to match in 'field'
-//  * @returns { object} - If found, the first object in the array to have the matching key/value pair. Returns , what a messspecified object
- */
+
 const removeObjectFromArray = (array, field, value) => {
   const idx = array.findIndex(i => i[field] === value)
   const newArray = remove(idx, 1, array)
